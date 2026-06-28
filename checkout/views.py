c1a8e96e63663357d6625a7932ea80e6d6a2a7be
Azamat Kashebayev <AKashebaev@gmail.com>
2026-06-28 20:39:32 +0500

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_live_51HG1pUDmfjpaTgJ1derEmswL1P1o7BWaEEOIllmxayHVKfVX845WvIvSAkcW2CftiAXJvNLrVhLahyds1Ndls70u00gSy0eBss',  # noqa
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
