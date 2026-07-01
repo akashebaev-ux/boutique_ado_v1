ac63665ac75b3bd9c83ac2d8dee7c64ab373ccdc
Azamat Kashebayev <AKashebaev@gmail.com>
2026-07-01 23:11:46 +0500

    Add ALLOWED_HOSTS, configure webhook URLs, and expand testing

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -27,7 +27,10 @@ SECRET_KEY = "django-insecure-+8&i^^x4h=b+3##cs4xi7#pniuljy&v84za%w43$f5ac_&53g5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',  # vs code preview
+    'localhost',  # listen for stripe webhooks
+]
 
 
 # Application definition
@@ -178,6 +181,7 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
